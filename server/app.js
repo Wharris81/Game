8ea7491bab79d7565d74a04e0b1fd5a2bfa3f117
Will Harris <will.harris@valtech.com>
2019-10-27 19:24:40 +0000

    Basic game set up, position not feeding through

--- server/app.js
+++ server/app.js
@@ -9,6 +9,25 @@ var position = {
 
 Socketio.on("connection", socket => {
     socket.emit("position", position);
+    socket.on("move", data => {
+        switch(data) {
+            case "left":
+                position.x -= 5;
+                Socketio.emit("position", position);
+            case "right":
+                position.x += 5;
+                Socketio.emit("position", position);
+                break;
+            case "up":
+                position.y -= 5;
+                Socketio.emit("position", position);
+                break;
+            case "dow":
+                position.y += 5;
+                Socketio.emit("position", position);
+                break;
+        }
+    })
 });
 
 Http.listen(3000, () => {
